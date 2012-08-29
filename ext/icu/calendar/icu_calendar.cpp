6b45c11969455ffe56a96b91c46810366b2f9763
Chris Bandy <bandy.chris@gmail.com>
2012-08-28 21:09:06 -0500

    Do not alias the ICU namespace

--- ext/icu/calendar/icu_calendar.cpp
+++ ext/icu/calendar/icu_calendar.cpp
@@ -6,12 +6,11 @@
 #include <rice/Module.hpp>
 #include <unicode/calendar.h>
 
-namespace ICU = icu;
 
 Rice::Object calendar_available_locales(Rice::Object /* class */)
 {
 	int32_t count = 0, i;
-	const ICU::Locale *locales = ICU::Calendar::getAvailableLocales(count);
+	const icu::Locale *locales = icu::Calendar::getAvailableLocales(count);
 	Rice::Array result;
 
 	for (i = 0; i < count; ++i)
@@ -25,6 +24,6 @@ void Init_icu_calendar()
 {
 	Rice::Module rb_mICU = Rice::define_module("ICU");
 
-	Rice::Data_Type<ICU::Calendar> rb_cCalendar = rb_mICU.define_class("Calendar")
+	Rice::Data_Type<icu::Calendar> rb_cCalendar = rb_mICU.define_class("Calendar")
 		.define_singleton_method("available_locales", &calendar_available_locales);
 }
