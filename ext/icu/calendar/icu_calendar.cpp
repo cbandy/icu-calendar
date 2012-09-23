65609d72e44191101d341f5a79a216e6e6f8ea5e
Chris Bandy <bandy.chris@gmail.com>
2012-09-23 16:07:13 -0500

    Other timezone lists

--- ext/icu/calendar/icu_calendar.cpp
+++ ext/icu/calendar/icu_calendar.cpp
@@ -50,6 +50,28 @@ Rice::Object to_ruby<icu::UnicodeString>(icu::UnicodeString const &x)
 	return result;
 }
 
+template<>
+Rice::Object to_ruby<icu::StringEnumeration*>(icu::StringEnumeration* const &x)
+{
+	const icu::UnicodeString *item;
+	Rice::Array result;
+	ErrorCode status;
+
+	item = x->snext(status);
+	status.assertSuccess();
+
+	while (item != NULL)
+	{
+		result.push(to_ruby(*item));
+
+		item = x->snext(status);
+		status.assertSuccess();
+	}
+
+	delete x;
+	return result;
+}
+
 Rice::Array calendar_available_locales(Rice::Object /* class */)
 {
 	int32_t count = 0, i;
@@ -72,25 +94,11 @@ Rice::String timezone_canonical_id(Rice::Object /* class */, Rice::String id)
 	return to_ruby(result);
 }
 
-Rice::Array timezone_enumeration(Rice::Object /* class */)
+Rice::Array country_timezones(Rice::Object /* class */, Rice::Object country)
 {
-	const icu::UnicodeString *timezone;
-	icu::StringEnumeration *timezones = icu::TimeZone::createEnumeration();
-	Rice::Array result;
-	ErrorCode status;
+	const char *str = country.is_nil() ? NULL : Rice::String(country).c_str();
 
-	timezone = timezones->snext(status);
-	status.assertSuccess();
-
-	while (timezone != NULL)
-	{
-		result.push(to_ruby(*timezone));
-
-		timezone = timezones->snext(status);
-		status.assertSuccess();
-	}
-
-	return result;
+	return to_ruby(icu::TimeZone::createEnumeration(str));
 }
 
 extern "C"
@@ -101,7 +109,13 @@ void Init_icu_calendar()
 	rb_cICUCalendar = rb_mICU.define_class("Calendar")
 		.define_singleton_method("available_locales", &calendar_available_locales)
 		.define_singleton_method("canonical_timezone_identifier", &timezone_canonical_id)
-		.define_singleton_method("timezones", &timezone_enumeration);
+		.define_singleton_method("country_timezones", &country_timezones)
+		.define_singleton_method("offset_timezones",
+				(icu::StringEnumeration* (*)(int32_t))
+				&icu::TimeZone::createEnumeration)
+		.define_singleton_method("timezones",
+				(icu::StringEnumeration* (*)())
+				&icu::TimeZone::createEnumeration);
 
 	rb_eICURuntimeError = rb_cICUCalendar.define_class("RuntimeError", rb_eRuntimeError);
 }
--- spec/calendar_spec.rb
+++ spec/calendar_spec.rb
@@ -34,6 +34,25 @@ module ICU
           Calendar.canonical_timezone_identifier('GMT+1:15').should == 'GMT+01:15'
         end
       end
+
+      describe 'country timezones' do
+        it 'returns a list of timezones associated with a country' do
+          Calendar.country_timezones('DE').should == ['Europe/Berlin']
+          Calendar.country_timezones('US').should include 'America/Chicago'
+          Calendar.country_timezones('CN').should_not include 'UTC'
+        end
+
+        it 'returns a list of timezones associated with no countries' do
+          Calendar.country_timezones(nil).should include 'UTC'
+        end
+      end
+
+      describe 'offset timezones' do
+        it 'returns a list of timezones for an offset in milliseconds' do
+          Calendar.offset_timezones(-10_800_000).should include 'BET'
+          Calendar.offset_timezones(3_600_000).should include 'Europe/Berlin'
+        end
+      end
     end
   end
 end
