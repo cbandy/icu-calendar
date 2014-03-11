87222ad7ae8c76b40d0cb5f00c0eec02cf8065d7
Chris Bandy <bandy.chris@gmail.com>
2014-03-11 06:21:26 +0000

    Find C++ headers by disabling language auto-detection
    GCC looks to the file extension when selecting the language, but Ruby's
    MakeMakefile always creates a C source file (conftest.c).

--- ext/icu/calendar/extconf.rb
+++ ext/icu/calendar/extconf.rb
@@ -1,24 +1,23 @@
 require 'mkmf-rice'
 
+def with_cc_cplusplus
+  original = $CFLAGS.dup
+  $CFLAGS << ' -x c++'
+  yield
+ensure
+  $CFLAGS = original
+end
+
 $defs.push('-DU_USING_ICU_NAMESPACE=0')
 
 idir, ldir = dir_config('icu')
 
-# terrible hack for mkmf C++ support
-begin
-  if ldir && $LIBPATH.last == ldir
-    $LIBPATH.pop
-    $DEFLIBPATH.unshift(ldir)
-  end
-
-  find_header('bits/c++config.h', '/usr/lib/gcc/x86_64-pc-linux-gnu/4.5.3/include/g++-v4/x86_64-pc-linux-gnu')
-  find_header('string', '/usr/lib/gcc/x86_64-pc-linux-gnu/4.5.3/include/g++-v4')
+with_cc_cplusplus do
+  find_header('unicode/calendar.h') || abort
+  find_header('unicode/timezone.h') || abort
 end
 
-have_header('unicode/calendar.h')
-have_header('unicode/timezone.h')
-
-have_library('icuuc')
-have_library('icui18n')
+have_library('icuuc',   'u_errorName', 'unicode/utypes.h') || abort
+have_library('icui18n', 'ucal_open',   'unicode/ucal.h')   || abort
 
 create_makefile('icu/icu_calendar')
