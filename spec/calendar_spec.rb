af73341a3d8caae639b23b39fd5217d27f1c2358
Chris Bandy <bandy.chris@gmail.com>
2014-03-12 01:54:09 +0000

    Switch to expectation syntax in specs

--- spec/calendar_spec.rb
+++ spec/calendar_spec.rb
@@ -5,52 +5,58 @@ require 'icu/calendar'
 module ICU
   describe Calendar do
     it 'defines a RuntimeError' do
-      Calendar::RuntimeError.new.should be_a ::RuntimeError
+      expect(Calendar::RuntimeError.new).to be_a(::RuntimeError)
     end
 
     describe 'available locales' do
-      subject { Calendar.available_locales }
+      subject(:locales) { Calendar.available_locales }
 
       it { should be_an Array }
       it { should_not be_empty }
-      its(:first) { should be_a String }
+
+      it 'contains Strings' do
+        expect(locales.first).to be_a(String)
+      end
     end
 
     describe 'timezones' do
-      subject { Calendar.timezones }
+      subject(:zones) { Calendar.timezones }
 
       it { should be_an Array }
       it { should_not be_empty }
-      its(:first) { should be_a String }
-      its(:'first.encoding') { should == Encoding::UTF_8 }
+
+      it 'contains UTF-8 Strings' do
+        expect(zones.first).to be_a(String)
+        expect(zones.first.encoding).to be(Encoding::UTF_8)
+      end
 
       describe 'canonical timezone identifier' do
         it 'returns a canonical system identifier' do
-          Calendar.canonical_timezone_identifier('GMT').should == 'Etc/GMT'
+          expect(Calendar.canonical_timezone_identifier('GMT')).to eq('Etc/GMT')
         end
 
         it 'returns a normalized custom identifier' do
-          Calendar.canonical_timezone_identifier('GMT-6').should == 'GMT-06:00'
-          Calendar.canonical_timezone_identifier('GMT+1:15').should == 'GMT+01:15'
+          expect(Calendar.canonical_timezone_identifier('GMT-6')).to eq('GMT-06:00')
+          expect(Calendar.canonical_timezone_identifier('GMT+1:15')).to eq('GMT+01:15')
         end
       end
 
       describe 'country timezones' do
         it 'returns a list of timezones associated with a country' do
-          Calendar.country_timezones('DE').should == ['Europe/Berlin']
-          Calendar.country_timezones('US').should include 'America/Chicago'
-          Calendar.country_timezones('CN').should_not include 'UTC'
+          expect(Calendar.country_timezones('DE')).to eq(['Europe/Berlin'])
+          expect(Calendar.country_timezones('US')).to include('America/Chicago')
+          expect(Calendar.country_timezones('CN')).to_not include('UTC')
         end
 
         it 'returns a list of timezones associated with no countries' do
-          Calendar.country_timezones(nil).should include 'UTC'
+          expect(Calendar.country_timezones(nil)).to include('UTC')
         end
       end
 
       describe 'offset timezones' do
         it 'returns a list of timezones for an offset in milliseconds' do
-          Calendar.offset_timezones(-10_800_000).should include 'BET'
-          Calendar.offset_timezones(3_600_000).should include 'Europe/Berlin'
+          expect(Calendar.offset_timezones(-10_800_000)).to include('BET')
+          expect(Calendar.offset_timezones(3_600_000)).to include('Europe/Berlin')
         end
       end
     end
